a41736078526adaf1dc9fa89637b51df3a50510c
Markus Iser <markus.iser@kit.edu>
2023-09-27 15:03:59 +0200

    fix

--- src/ipasir2.cpp
+++ src/ipasir2.cpp
@@ -257,7 +257,7 @@ extern "C" {
         }
     }
 
-    ipasir2_errorcode ipasir2_add(void* solver, int32_t const* clause, int32_t len) {
+    ipasir2_errorcode ipasir2_add(void* solver, int32_t const* clause, int32_t len, ipasir2_redundancy type) {
         for (int i = 0; i < len; i++) {
             ((SolverWrapper*)solver)->add(clause[i]);
         }
